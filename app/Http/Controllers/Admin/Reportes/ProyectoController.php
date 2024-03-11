20e8efb8d02c508b575c55d2b59f6b3adfade629
BrunoPM2001 <brunoporras2001@gmail.com>
2024-03-11 16:04:01 -0500

    Some migrations related to Proyecto added

---
+++ app/Http/Controllers/Admin/Reportes/ProyectoController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Reportes;
+
+use App\Http\Controllers\Controller;
+use Barryvdh\DomPDF\Facade\Pdf;
+use Illuminate\Support\Facades\DB;
+
+class ProyectoController extends Controller {
+  public function reporte($tipo, $facultad, $periodo) {
+  }
+}
--- database/migrations/2024_03_04_190755_create_proyecto_integrante_table.php
+++ database/migrations/2024_03_04_190755_create_proyecto_integrante_table.php
@@ -12,12 +12,34 @@ return new class extends Migration {
     Schema::create('Proyecto_integrante', function (Blueprint $table) {
       $table->id();
       $table->unsignedBigInteger('proyecto_id');
-      $table->unsignedBigInteger('proyecto_integrante_tipo')->nullable();
+      $table->unsignedBigInteger('proyecto_integrante_tipo_id')->nullable();
       $table->unsignedBigInteger('grupo_id')->nullable();
       $table->unsignedBigInteger('grupo_integrante_id')->nullable();
       $table->unsignedBigInteger('investigador_id')->nullable();
       $table->string('condicion')->nullable();
-      //  TODO - Completar campos en base a lo que diga Max
+      $table->string('tipo_tesista', 50)->nullable();
+      $table->string('tipo_tesis', 150)->nullable();
+      $table->text('titulo_tesis')->nullable();
+      $table->text('responsabilidad')->nullable();
+      $table->string('contribucion')->nullable();
+      $table->string('condicion_grupo')->nullable();
+      $table->string('tipo_investigador')->nullable();
+      $table->boolean('excluido')->nullable();
+      $table->date('fecha_excluido')->nullable();
+      $table->string('res_dec_excluido')->nullable();
+      $table->date('res_dec_fecha_excluido')->nullable();
+      $table->string('res_rec_excluido')->nullable();
+      $table->date('res_rec_fecha_excluido')->nullable();
+      $table->text('info_exluido')->nullable();
+      $table->boolean('estado')->nullable();
+      $table->timestamps();
+
+      //  Fks
+      $table->foreign('proyecto_id')->references('id')->on('Proyecto');
+      $table->foreign('proyecto_integrante_tipo_id')->references('id')->on('Proyecto_integrante_tipo');
+      $table->foreign('grupo_id')->references('id')->on('Grupo');
+      $table->foreign('grupo_integrante_id')->references('id')->on('Grupo_integrante');
+      $table->foreign('investigador_id')->references('id')->on('Usuario_investigador');
     });
   }
 
