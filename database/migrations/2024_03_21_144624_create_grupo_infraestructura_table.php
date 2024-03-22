c44a85d3788ea22e8a2d856e1b0325a7a844b146
BrunoPM2001 <brunoporras2001@gmail.com>
2024-03-22 15:54:35 -0500

    New migration

--- database/migrations/2024_03_21_144624_create_grupo_infraestructura_table.php
+++ database/migrations/2024_03_21_144624_create_grupo_infraestructura_table.php
@@ -9,7 +9,27 @@ return new class extends Migration {
    * Run the migrations.
    */
   public function up(): void {
+    //  TODO - En la tabla de laboratorio registrar los laboratorios de esta tabla - migrar archivos a minio
     Schema::create('Grupo_infraestructura', function (Blueprint $table) {
+      $table->id();
+      $table->unsignedBigInteger('grupo_id');
+      $table->unsignedBigInteger('laboratorio_id')->nullable();
+      $table->string('categoria', 50);
+      $table->string('codigo', 20)->nullable();
+      $table->string('codigo')->nullable();
+      $table->string('nombre')->nullable();
+      $table->text('descripcion')->nullable();
+      $table->decimal('area_mt2', 10)->nullable();
+      $table->string('res_decanal')->nullable();
+      $table->text('contacto')->nullable();
+      $table->string('ubicacion')->nullable();
+      $table->decimal('valor_estimado', 10)->nullable();
+      $table->unsignedBigInteger('parent_id')->nullable();
+      $table->timestamps();
+
+      //  Fks
+      $table->foreign('grupo_id')->references('id')->on('Grupo');
+      $table->foreign('laboratorio_id')->references('id')->on('Laboratorio');
     });
   }
 
@@ -17,8 +37,6 @@ return new class extends Migration {
    * Reverse the migrations.
    */
   public function down(): void {
-    Schema::drop('Grupo_infraestructura', function (Blueprint $table) {
-      //
-    });
+    Schema::drop('Grupo_infraestructura');
   }
 };
