89d30bbc485d6bc07fdd05f9f8a8d144f3237bea
BrunoPM2001 <brunoporras2001@gmail.com>
2024-03-29 09:37:55 -0500

    New migration done and test

--- database/migrations/2024_03_27_153219_create_informe_tecnico_table.php
+++ database/migrations/2024_03_27_153219_create_informe_tecnico_table.php
@@ -10,7 +10,6 @@ return new class extends Migration {
    */
   public function up(): void {
     Schema::create('Informe_tecnico', function (Blueprint $table) {
-      //  TODO - Completar campos
       $table->id();
       $table->unsignedBigInteger('proyecto_id');
       $table->unsignedBigInteger('informe_tipo_id')->nullable();
@@ -22,6 +21,28 @@ return new class extends Migration {
       $table->date('fecha_evento')->nullable();
       $table->date('fecha_informe_tecnico')->nullable();
       $table->mediumText('objetivos_taller')->nullable();
+      $table->text('resultados_taller')->nullable();
+      $table->text('propuestas_taller')->nullable();
+      $table->text('conclusion_taller')->nullable();
+      $table->text('recomendacion_taller')->nullable();
+      $table->text('asistencia_taller')->nullable();
+      $table->mediumText('infinal1')->nullable();
+      $table->mediumText('infinal2')->nullable();
+      $table->mediumText('infinal3')->nullable();
+      $table->mediumText('infinal4')->nullable();
+      $table->mediumText('infinal5')->nullable();
+      $table->mediumText('infinal6')->nullable();
+      $table->mediumText('infinal7')->nullable();
+      $table->mediumText('infinal8')->nullable();
+      $table->mediumText('infinal9')->nullable();
+      $table->mediumText('infinal10')->nullable();
+      $table->mediumText('infinal11')->nullable();
+      $table->text('observaciones')->nullable();
+      $table->text('observaciones_admin')->nullable();
+      $table->tinyInteger('estado')->default(0);
+      $table->date('fecha_envio')->nullable();
+      $table->string('estado_trabajo')->nullable();
+      $table->timestamps();
     });
   }
 
