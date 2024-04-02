ef5c56b2852618f79baedaca009dc80281021871
BrunoPM2001 <brunoporras2001@gmail.com>
2024-04-02 12:15:30 -0500

    Working on a new migration, stop bcz some questions about columns

--- database/migrations/2024_04_02_120239_create_publicacion_proyecto_table.php
+++ database/migrations/2024_04_02_120239_create_publicacion_proyecto_table.php
@@ -4,25 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::table('Publicacion_proyecto', function (Blueprint $table) {
-            //
-        });
-    }
+return new class extends Migration {
+  /**
+   * Run the migrations.
+   */
+  public function up(): void {
+    Schema::create('Publicacion_proyecto', function (Blueprint $table) {
+      //  Todo verificar las FKS
+      $table->id();
+      $table->unsignedBigInteger('publicacion_id');
+      $table->unsignedBigInteger('proyecto_id');
+      $table->tinyInteger('estado');
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('Publicacion_proyecto', function (Blueprint $table) {
-            //
-        });
-    }
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void {
+    Schema::drop('Publicacion_proyecto');
+  }
 };
