128d5b89422c4bcf212c4db092579c03bd7c4982
BrunoPM2001 <brunoporras2001@gmail.com>
2024-04-09 10:36:24 -0500

    New migration

---
+++ database/migrations/2024_04_08_154051_create_publicacion_revista_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+  /**
+   * Run the migrations.
+   */
+  public function up(): void {
+    Schema::create('Publicacion_revista', function (Blueprint $table) {
+      $table->id();
+      $table->string('issn', 20)->nullable();
+      $table->string('issne', 100)->nullable();
+      $table->string('revista', 300)->nullable();
+      $table->string('casa', 300)->nullable();
+      $table->integer('isi')->nullable();
+      $table->string('pais', 50)->nullable();
+      $table->string('cobertura', 50)->nullable();
+      $table->boolean('estado')->nullable();
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void {
+    Schema::drop('Publicacion_revista');
+  }
+};
