c2bdf600bd55b98efd9700110a9cde5e924c634e
BrunoPM2001 <brunoporras2001@gmail.com>
2024-05-17 16:04:47 -0500

    Preparing to change handle of search investigador

--- app/Http/Controllers/Admin/Estudios/ConvocatoriasController.php
+++ app/Http/Controllers/Admin/Estudios/ConvocatoriasController.php
@@ -7,6 +7,99 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class ConvocatoriasController extends Controller {
+
+  public function createConvocatoria(Request $request) {
+    //  Convocatoria de registro
+    $id = DB::table('Convocatoria')
+      ->insertGetId([
+        'tipo' => $request->tipo,
+        'periodo' => $request->periodo,
+        'evento' => 'registro',
+        'estado' => 1,
+        'convocatoria' => $request->convocatoria,
+        'fecha_inicial' => $request->fecha_inicial,
+        'fecha_final' => $request->fecha_final,
+        'fecha_corte' => $request->fecha_corte
+      ]);
+
+    //  Convocatoria calendario
+    DB::table('Convocatoria')
+      ->insert([
+        'parent_id' => $id,
+        'evento' => 'calendario',
+        'fecha_inicial' => $request->fecha_inicial_calendario,
+        'fecha_final' => $request->fecha_final_calendario,
+      ]);
+
+    //  Convocatoria evaluacion
+    DB::table('Convocatoria')
+      ->insert([
+        'parent_id' => $id,
+        'evento' => 'evaluacion',
+        'fecha_inicial' => $request->fecha_inicial_evaluacion,
+        'fecha_final' => $request->fecha_final_evaluacion,
+      ]);
+
+    return ['message' => 'success', 'detail' => 'Convocatoria creada'];
+  }
+
+  public function updateConvocatoria(Request $request) {
+    //  Convocatoria de registro
+    DB::table('Convocatoria')
+      ->where('id', '=', $request->id)
+      ->update([
+        'periodo' => $request->periodo,
+        'convocatoria' => $request->convocatoria,
+        'fecha_inicial' => $request->fecha_inicial,
+        'fecha_final' => $request->fecha_final,
+        'fecha_corte' => $request->fecha_corte
+      ]);
+
+    //  Convocatoria calendario
+    DB::table('Convocatoria')
+      ->where('parent_id', '=', $request->id)
+      ->where('evento', '=', 'calendario')
+      ->updateOrInsert([
+        'parent_id' => $request->id,
+        'evento' => 'calendario',
+        'fecha_inicial' => $request->fecha_inicial_calendario,
+        'fecha_final' => $request->fecha_final_calendario,
+      ]);
+
+    //  Convocatoria evaluacion
+    DB::table('Convocatoria')
+      ->where('parent_id', '=', $request->id)
+      ->where('evento', '=', 'evaluacion')
+      ->updateOrInsert([
+        'parent_id' => $request->id,
+        'evento' => 'evaluacion',
+        'fecha_inicial' => $request->fecha_inicial_evaluacion,
+        'fecha_final' => $request->fecha_final_evaluacion,
+      ]);
+
+    return ['message' => 'info', 'detail' => 'Convocatoria actualizada'];
+  }
+
+  public function deleteConvocatoria(Request $request) {
+    DB::table('Convocatoria')
+      ->select(
+        'id',
+        'tipo',
+        'evento',
+        'fecha_inicial',
+        'fecha_final',
+        'fecha_corte',
+        'periodo',
+        'convocatoria',
+        'estado'
+      )
+      ->where('id', '=', $request->id)
+      ->orWhere('parent_id', '=', $request->id)
+      ->delete();
+
+    return ['message' => 'info', 'detail' => 'Convocatoria eliminada completamente'];
+  }
+
   public function listarConvocatorias() {
     $convocatorias = DB::table('Convocatoria')
       ->select(
@@ -114,97 +207,5 @@ class ConvocatoriasController extends Controller {
       ->get();
 
     return ['evaluacion' => $evaluacion, 'criterios' => $criterios];
-  }
-
-  public function deleteConvocatoria(Request $request) {
-    DB::table('Convocatoria')
-      ->select(
-        'id',
-        'tipo',
-        'evento',
-        'fecha_inicial',
-        'fecha_final',
-        'fecha_corte',
-        'periodo',
-        'convocatoria',
-        'estado'
-      )
-      ->where('id', '=', $request->id)
-      ->orWhere('parent_id', '=', $request->id)
-      ->delete();
-
-    return ['message' => 'info', 'detail' => 'Convocatoria eliminada completamente'];
-  }
-
-  public function updateConvocatoria(Request $request) {
-    //  Convocatoria de registro
-    DB::table('Convocatoria')
-      ->where('id', '=', $request->id)
-      ->update([
-        'periodo' => $request->periodo,
-        'convocatoria' => $request->convocatoria,
-        'fecha_inicial' => $request->fecha_inicial,
-        'fecha_final' => $request->fecha_final,
-        'fecha_corte' => $request->fecha_corte
-      ]);
-
-    //  Convocatoria calendario
-    DB::table('Convocatoria')
-      ->where('parent_id', '=', $request->id)
-      ->where('evento', '=', 'calendario')
-      ->updateOrInsert([
-        'parent_id' => $request->id,
-        'evento' => 'calendario',
-        'fecha_inicial' => $request->fecha_inicial_calendario,
-        'fecha_final' => $request->fecha_final_calendario,
-      ]);
-
-    //  Convocatoria evaluacion
-    DB::table('Convocatoria')
-      ->where('parent_id', '=', $request->id)
-      ->where('evento', '=', 'evaluacion')
-      ->updateOrInsert([
-        'parent_id' => $request->id,
-        'evento' => 'evaluacion',
-        'fecha_inicial' => $request->fecha_inicial_evaluacion,
-        'fecha_final' => $request->fecha_final_evaluacion,
-      ]);
-
-    return ['message' => 'info', 'detail' => 'Convocatoria actualizada'];
-  }
-
-  public function createConvocatoria(Request $request) {
-    //  Convocatoria de registro
-    $id = DB::table('Convocatoria')
-      ->insertGetId([
-        'tipo' => $request->tipo,
-        'periodo' => $request->periodo,
-        'evento' => 'registro',
-        'estado' => 1,
-        'convocatoria' => $request->convocatoria,
-        'fecha_inicial' => $request->fecha_inicial,
-        'fecha_final' => $request->fecha_final,
-        'fecha_corte' => $request->fecha_corte
-      ]);
-
-    //  Convocatoria calendario
-    DB::table('Convocatoria')
-      ->insert([
-        'parent_id' => $id,
-        'evento' => 'calendario',
-        'fecha_inicial' => $request->fecha_inicial_calendario,
-        'fecha_final' => $request->fecha_final_calendario,
-      ]);
-
-    //  Convocatoria evaluacion
-    DB::table('Convocatoria')
-      ->insert([
-        'parent_id' => $id,
-        'evento' => 'evaluacion',
-        'fecha_inicial' => $request->fecha_inicial_evaluacion,
-        'fecha_final' => $request->fecha_final_evaluacion,
-      ]);
-
-    return ['message' => 'success', 'detail' => 'Convocatoria creada'];
   }
 }
