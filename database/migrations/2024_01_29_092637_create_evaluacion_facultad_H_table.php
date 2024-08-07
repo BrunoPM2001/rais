f5877b751ee8392127a5e591276dd5908e4e2be5
Bruno PM <brunoporras2001@gmail.com>
2024-08-07 10:11:37 -0500

    Changing column name in a migration - fixed error

--- database/migrations/2024_01_29_092637_create_evaluacion_facultad_H_table.php
+++ database/migrations/2024_01_29_092637_create_evaluacion_facultad_H_table.php
@@ -31,6 +31,6 @@ return new class extends Migration {
    * Reverse the migrations.
    */
   public function down(): void {
-    Schema::drop('Evaluacion_facultad');
+    Schema::drop('Evaluacion_facultad_H');
   }
 };
